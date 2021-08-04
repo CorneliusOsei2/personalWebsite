ff51ebe560541ab119b0b3f8451280b9d21a6888
Cornelius <oseiowusuboatengcornelius6@gmail.com>
2021-08-04 00:53:59 -0400

    One project added for testing

--- src/Components/project/Project.jsx
+++ src/Components/project/Project.jsx
@@ -100,6 +100,39 @@ function Project() {
                     </div>
                 </div>
 
+                <div className="proj">
+                    <div className="top">
+                    <div class="image-box">
+                        <div class="image">
+                        <img src="Images/person.jpg" alt="" />
+                        </div>
+                        <div class="image">
+                        <img src="Images/person.jpg" alt="" />
+                        </div>
+                        <div class="image">
+                        <img src="Images/person.jpg" alt="" />
+                        </div>
+                        <div class="image">
+                        <img src="Images/person.jpg" alt="" />
+                        </div>
+                        </div>
+                    </div>
+                    <div className="center">
+                        <div className="title">Here</div>
+                    </div>
+                    <div className="bottom">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+                        
+                        <div className="tech">
+                            <div>Languages and Technologies Used:</div>
+                            Python
+                        </div>
+                    </div>
+                </div>
+
+                
+
             </div>
             
         </div>
