bea7972d6ebd501397eb99c69b8139f3ae9f8f66
Cornelius Osei Owusu Boateng <78667952+CorneliusOsei2@users.noreply.github.com>
2021-08-19 14:47:16 -0400

    Data upload I

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -14,7 +14,7 @@ function Intro() {
         init(titleRef.current, {
             typeSpeed: 40,
             showCursor: false,
-            strings: ['Computer Science Student'],
+            strings: ['Cornelius'],
             backDelay: 50000,
         })
 
@@ -22,7 +22,7 @@ function Intro() {
             typeSpeed: 40,
             showCursor: false,
             backDelay: 50000,
-            strings: ['I am a Computer Science Student at Cornell University passionate about solving problems with efficient means!']
+            strings: ['A Computer Science Student at Cornell University passionate about solving problems with efficient means! I like learning new stuff and writing code']
         })
     }, [])
 
@@ -32,12 +32,13 @@ function Intro() {
             <div className="left">
                 <div className="imgContainer">
                     <img src="" alt="" />
+                    <p>An amazing picture is on its way</p>
                 </div>
 
                 <div className="itemContainer">
-                    <a aria-label="false" target="_blank" tabIndex="0" rel="noopener noreferr" href="https://www.linkedin.com/in/cornelius-boateng-29b5b01a5"><LinkedIn className="icon"/></a>
-                    <a aria-label="false" target="_blank" tabIndex="0" rel="noopener noreferr" href="https://github.com/CorneliusOsei2"><GitHub className="icon" /></a>
-                    <a aria-label="false" target="_blank" tabIndex="0" rel="noopener noreferr" href="https://www.linkedin.com/in/cornelius-boateng-29b5b01a5"><AssignmentInd className="icon" /></a>
+                    <a aria-label="false" target="_blank" tabIndex="0" rel="noopener noreferrer" href="https://www.linkedin.com/in/cornelius-boateng-29b5b01a5"><LinkedIn className="icon"/></a>
+                    <a aria-label="false" target="_blank" tabIndex="0" rel="noopener noreferrer" href="https://github.com/CorneliusOsei2"><GitHub className="icon" /></a>
+                    <a aria-label="false" target="_blank" tabIndex="0" rel="noopener noreferrer" href="https://www.linkedin.com/in/cornelius-boateng-29b5b01a5"><AssignmentInd className="icon" /></a>
                 </div>
             </div>
 
@@ -45,10 +46,10 @@ function Intro() {
                 <Particles params={particlesConfig} className='particles-container'/>
                 <div className="wrapper">
                     <h4>Hi there, I'm</h4>
-                    <h2>Cornelius</h2>
-                    <h4 ref={titleRef}></h4>
+                    <h2 ref={titleRef} ></h2>
 
                     <p ref={descRef}></p>
+                    <p className="emoji">🧑🏾‍💻</p>
 
                 </div>
 
--- src/Components/project/Project.jsx
+++ src/Components/project/Project.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './project.scss'
 
-// import snake_game from '../../media/snake-game.mov'
+import roster_automator from '../../media/roster-automator.mp4'
+import snake_game from '../../media/snake-game.mp4'
+
 
 function Project() {
     return (
@@ -20,15 +22,44 @@ function Project() {
                     </div>
                     
                     <div className="bottom">
-                        <div className="title">Name</div>
+                        <div className="title">StudyPot Learning Website</div>
                             <div className="description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+                                Developed to host organized learning materials on the medical sciences
+                                to expose students to selected topics and help them make up for class lessons lost to the pandemic.
                             </div>
                         
                         <div className="tech">
                             <div>Built with:</div>
-                            Python
+                            Node JS |MongoDB |EJS |CSS3 |Passport | Cloudinary | Bootstrap 5
+                            <br />
+                            <a className="visit-btn" href="http://www.studypot.org"> Visit </a>
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div className="proj">
+                    <div className="top">
+                        <div className="vid">
+                            <video className="vid" autoPlay loop muted>
+                                <source src={roster_automator} type="video/mp4"/>
+                            </video>
+                        </div>
+
+                    </div>
+                    
+                    <div className="bottom">
+                        <div className="title">Cornell Roster Automator</div>
+                            <div className="description">
+                                Developed to host organized learning materials on the medical sciences
+                                to expose students to selected topics and help them make up for class lessons lost to the pandemic.
+                            </div>
+                        
+                        <div className="tech">
+                            <div>Built with:</div>
+                            Node JS |MongoDB |EJS |CSS3 |Passport | Cloudinary | Bootstrap 5
+                            <br />
+                            <a className="visit-btn" href="http://www.studypot.org"> Demo </a>
                         </div>
                     </div>
                 </div>
@@ -36,47 +67,25 @@ function Project() {
                 <div className="proj">
                     <div className="top">
                         <div className="vid">
-                            {/* <video className="vid" autoPlay loop muted>
+                            <video className="vid" autoPlay loop muted>
                                 <source src={snake_game} type="video/mp4"/>
-                            </video> */}
+                            </video>
                         </div>
 
                     </div>
                     
                     <div className="bottom">
-                        <div className="title">Name</div>
+                        <div className="title">Box Snake Game</div>
                             <div className="description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+                                A 2D 'Snake' Game built to have practice with game development. Has keyboard support for the arrow
+                                and 'ASDW' keys. Cool sound effect and background (one of my night shots) added!
                             </div>
                         
                         <div className="tech">
                             <div>Built with:</div>
-                            Python
-                        </div>
-                    </div>
-                </div>
-
-                <div className="proj">
-                    <div className="top">
-                        <div className="vid">
-                            {/* <video className="vid" autoPlay loop muted>
-                                <source src={snake_game} type="video/mp4"/>
-                            </video> */}
-                        </div>
-
-                    </div>
-                    
-                    <div className="bottom">
-                        <div className="title">Name</div>
-                            <div className="description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-                                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi  
-                            </div>
-                        
-                        <div className="tech">
-                            <div>Built with:</div>
-                            Python
+                            HTML | CSS | JavaScript | Firebase
+                            <br />
+                            <a className="visit-btn" href="https://box-snake-game-e79e8.web.app"> Try it! </a>
                         </div>
                     </div>
                 </div>
