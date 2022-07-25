da8b9eff4b67ada717cccb89711f6320246419fa
CorneliusOsei2 <oseiowusuboatengcornelius6@gmail.com>
2022-07-24 23:14:37 -0700

    Added focus website project

--- src/Components/project/Project.jsx
+++ src/Components/project/Project.jsx
@@ -4,6 +4,7 @@ import './project.scss'
 import roster_automator from '../../media/roster-automator.mp4'
 import snake_game from '../../media/snake-game.mp4'
 import study_pot from '../../media/study-pot.mp4'
+import focus from '../../media/focus.mp4'
 
 function Project() {
     return (
@@ -25,12 +26,12 @@ function Project() {
                         <div className="title">StudyPot Learning Website</div>
                             <div className="description">
                                 Developed to host organized learning materials on the medical sciences
-                                to expose students to selected topics and help them make up for class lessons lost to the pandemic.
+                                to expose students to selected topics to make up for class lessons lost to the pandemic.
                             </div>
                         
                         <div className="tech">
                             <div>Built with:</div>
-                            Node JS |MongoDB |EJS |CSS3 |Passport | Cloudinary | Bootstrap 5
+                            NodeJS |MongoDB |EJS |CSS3 |Passport | AWS S3 | Bootstrap
                             <br />
                             <a target="_blank" tabIndex="0" rel="noopener noreferrer" className="visit-btn" href="http://www.studypot.org"> Visit </a>
                         </div>
@@ -85,6 +86,32 @@ function Project() {
                             HTML | CSS | JavaScript | Firebase
                             <br />
                             <a target="_blank" tabIndex="0" rel="noopener noreferrer" className="visit-btn" href="https://box-snake-game-e79e8.web.app"> Try it! </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="proj">
+                    <div className="top">
+                        <div className="vid">
+                            <video className="vid" playsInline autoPlay loop muted defaultmuted>
+                                <source src={focus} type="video/mp4"/>
+                            </video>
+                        </div>
+
+                    </div>
+                    
+                    <div className="bottom">
+                        <div className="title">Focus Website (In dev)</div>
+                            <div className="description">
+                                Fully-responsive website for Focus Inc. in Ghana.
+                                Company is focused on enhancing food accessibility in Africa and advertising farm produce!
+                            </div>
+                        
+                        <div className="tech">
+                            <div>Built with:</div>
+                            Django | SQLite3 | Bootstrap | AWS S3 | Heroku
+                            <br />
+                            <a target="_blank" tabIndex="0" rel="noopener noreferrer" className="visit-btn" href="https://focus-website-general.herokuapp.com/"> Explore! </a>
                         </div>
                     </div>
                 </div>
